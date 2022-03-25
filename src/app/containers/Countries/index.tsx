5be785f864a3df417e6915c582ce6eb75aea6576
ksample89 <kelly.sample@carerev.com>
2022-03-25 12:20:48 -0400

    prettier rc, Countries file, ReadMe instructions

--- src/app/containers/Countries/index.tsx
+++ src/app/containers/Countries/index.tsx
@@ -1,53 +1,41 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components/macro';
-import { useSelector, useDispatch } from 'react-redux';
-import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
-import { saga } from './saga';
-import { key, countriesReducer } from './reducer';
-import { actions } from './actions';
-import { selectCountries, selectLoading, selectError } from './selectors';
-import { LoadingIndicator } from 'app/components/LoadingIndicator';
-import { Link } from 'app/components/Link';
-import { PageWrapper } from 'app/components/PageWrapper';
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors'
+import { saga } from './saga'
+import { key, countriesReducer } from './reducer'
+import { actions } from './actions'
+import { selectCountries, selectLoading, selectError } from './selectors'
+import { LoadingIndicator } from 'app/components/LoadingIndicator'
+import { Link } from 'app/components/Link'
+import { PageWrapper } from 'app/components/PageWrapper'
 
-export function Countries() {
-  useInjectReducer({ key: key, reducer: countriesReducer });
-  useInjectSaga({ key: key, saga });
+export const Countries = () => {
+  useInjectReducer({ key: key, reducer: countriesReducer })
+  useInjectSaga({ key: key, saga })
 
-  const countries = useSelector(selectCountries);
-  const isLoading = useSelector(selectLoading);
-  const error = useSelector(selectError);
+  const countries = useSelector(selectCountries)
+  const isLoading = useSelector(selectLoading)
+  const error = useSelector(selectError)
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(actions.fetchCountries());
-  }, [dispatch]);
+    dispatch(actions.fetchCountries())
+  }, [dispatch])
 
   return (
     <PageWrapper>
       <h1>Countries</h1>
       {isLoading && <LoadingIndicator small />}
-      {countries?.length > 0 ? (
-        <List>
+      {countries?.length > 0 && (
+        <ul>
           {countries.map(country => (
-            <Country key={country.id}>
+            <li key={country.id}>
               <Link to={`/country/${country.id}`}>{country.name}</Link>
-            </Country>
+            </li>
           ))}
-        </List>
-      ) : error ? (
-        <ErrorText>{error}</ErrorText>
-      ) : null}
+        </ul>
+      )}
+      {error && <div>{error}</div>}
     </PageWrapper>
-  );
+  )
 }
-
-const Country = styled.li`
-  color: blue;
-`;
-
-const ErrorText = styled.span`
-  color: ${p => p.theme.text};
-`;
-
-const List = styled.div``;
